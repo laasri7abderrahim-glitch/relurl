077db697246dd9ec0e9d98999f0309b71484bfb5
Forji <forji@relurl.com>
2026-07-29 01:45:25 +0100

    fix: list individual sitemap URLs in robots.txt (sitemap.xml index broken)

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ["/dashboard/", "/admin/", "/api/", "/_next/", "/p/", "/login", "/register", "/forgot-password", "/reset-password"],
       },
     ],
-    sitemap: "https://relurl.com/sitemap.xml",
+    sitemap: ["https://relurl.com/sitemap/translated.xml", "https://relurl.com/sitemap/landing.xml", "https://relurl.com/sitemap/tools.xml", "https://relurl.com/sitemap/static.xml"],
   }
 }
